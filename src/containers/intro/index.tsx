8ff6ff6b5df4c441666abcc5c2953cce093be965
Clément Prod'homme <contact@clementprodhomme.fr>
2023-12-05 13:51:10 +0100

    Adjust the intro

--- src/containers/intro/index.tsx
+++ src/containers/intro/index.tsx
@@ -11,6 +11,13 @@ const ParticlesLogo = () => {
       </div>
       <div className="container relative z-10 flex h-screen flex-col items-center justify-center pb-20 pt-40 sm:pb-0 sm:pt-20">
         <IntroContent />
+        <img
+          className="absolute bottom-12 left-1/2 -translate-x-1/2"
+          src="/images/arrow-down.svg"
+          width={15}
+          height={28}
+          alt=""
+        />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -25,26 +25,25 @@ const config: Config = {
     },
     extend: {
       colors: {
-        "dark-gray": "#222",
-        "medium-gray": "#7E7E7E",
-        "light-gray": "#D8D8D8",
-        "primary-green": "#03755E",
-        "primary-green-dark": "#015645",
+        'dark-gray': '#222',
+        'medium-gray': '#7E7E7E',
+        'light-gray': '#D8D8D8',
+        'primary-green': '#03755E',
+        'primary-green-dark': '#015645',
       },
       fontFamily: {
-        open: ["var(--font-open)"],
-        cardo: ["var(--font-cardo)"],
+        open: ['var(--font-open)'],
+        cardo: ['var(--font-cardo)'],
       },
       fontSize: {
         '3.5xl': ['32px', '48px'],
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
-export default config
+};
+export default config;
