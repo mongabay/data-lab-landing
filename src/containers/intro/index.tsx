061a93717627bc7c3d1caf6f2aa369bcff49a153
María <maluenarod@gmail.com>
2026-09-10 10:05:18 +0200

    chore: apply Tailwind 4 class order to intro content (missed by eslint cache)

--- src/containers/intro/index.tsx
+++ src/containers/intro/index.tsx
@@ -9,7 +9,7 @@ const ParticlesLogo = () => {
       <div className="absolute h-screen w-full">
         <Circles />
       </div>
-      <div className="container relative z-10 my-auto flex h-screen items-center pb-20 pt-40 sm:pb-0 sm:pt-20">
+      <div className="relative z-10 container my-auto flex h-screen items-center pt-40 pb-20 sm:pt-20 sm:pb-0">
         <IntroContent />
       </div>
     </div>
